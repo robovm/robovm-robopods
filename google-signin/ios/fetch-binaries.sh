f0c4a0a146e4922fe69c09ba2bc3ba6187d27c56
BlueRiverInteractive <blueriverteam@gmail.com>
2016-02-25 17:04:08 +0100

    Add fetch-binaries script for development purposes.

---
+++ google-signin/ios/fetch-binaries.sh
@@ -0,0 +1,17 @@
+#/bin/sh
+
+# This script fetches the HeyZap SDK for development purposes
+# You need to agree to HeyZap's Terms of Services before
+# being allowed to use the SDK
+set -e
+VERSION=2_4_0
+rm -f google_signin_sdk_$VERSION.zip
+rm -rf signin
+mkdir signin
+curl -O https://developers.google.com/identity/sign-in/ios/sdk/google_signin_sdk_$VERSION.zip
+tar xzf google_signin_sdk_$VERSION.zip -C signin
+rm -rf libs/
+mkdir libs
+cp -r signin/google_signin_sdk_$VERSION/GoogleSignIn.framework libs/
+rm google_signin_sdk_$VERSION.zip
+rm -r signin
